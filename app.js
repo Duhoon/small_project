8ea3b01d486d17f8dbe3e065ec045c7ba62c6e86
Duhoon <eclip6@naver.com>
2019-07-19 16:16:22 +0900

    just complete

--- app.js
+++ app.js
@@ -10,9 +10,24 @@ res.sendFile(path.join(__dirname + '/main.html'));
     console.log(req)
 });
 
-router.get('/dumain.html', function (req, res) {
-res.sendFile(path.join(__dirname + '/.html'));
+router.get('/duindex', function (req, res) {
+res.sendFile(path.join(__dirname + '/views/kdh/index.html'));
 //__dirname : It will resolve to your project folder.
 });
+
+router.get('/dunottice', function (req, res) {
+res.sendFile(path.join(__dirname + '/views/kdh/nottice.html'));
+//__dirname : It will resolve to your project folder.
+});
+
+router.get('/dusign', function (req, res) {
+res.sendFile(path.join(__dirname + '/views/kdh/sign.html'));
+//__dirname : It will resolve to your project folder.
+});
+
+router.get('/getsign', function(req, res) {
+res
+}
+);
 
 app.use('/', router);
