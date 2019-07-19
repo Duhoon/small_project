0dc32f0c5a7be2e4109038da73854df01634e7d9
Duhoon <eclip6@naver.com>
2019-07-19 16:43:37 +0900

    modify 2

--- app.js
+++ app.js
@@ -1,8 +1,11 @@
-app.use(express.static('public'))
+const express = require('express')
+const app = express()
+const port = 3000
 
 const path = require('path');
 const router = express.Router();
 
+app.use(express.static('public'))
 
 router.get('/', function (req, res) {
 res.sendFile(path.join(__dirname + '/main.html'));
@@ -25,9 +28,10 @@ res.sendFile(path.join(__dirname + '/views/kdh/sign.html'));
 //__dirname : It will resolve to your project folder.
 });
 
-router.get('/getsign', function(req, res) {
-res
-}
-);
+router.get('/dusignup', function (req, res) {
+    console.log(req.query);
+});
 
 app.use('/', router);
+
+app.listen(port, () => console.log(`Example app listening on port ${port}!`))
